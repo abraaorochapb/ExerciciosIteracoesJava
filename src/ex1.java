6d5c2ddc7c27b213d9b7e98c2b4c213c6ec1536d
abraaorochapb <abraaorochapb@gmail.com>
2023-08-14 14:59:32 -0300

    Pequena correção no ex 1, ao invés de contabilizar o numero, iremos imprimir ele

--- src/ex1.java
+++ src/ex1.java
@@ -5,14 +5,14 @@ public class ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite um número: ");
+        System.out.print("Digite um número: ");
         int n = sc.nextInt();
 
         int impares = 0;
 
         for (int i = 1; i <= n; i++) {
             if (i % 2 != 0) {
-                impares++;
+                System.out.println(i);
             }
         }
 
